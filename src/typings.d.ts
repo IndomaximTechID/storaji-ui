7393547269782876b77e1dfd26261c431b874033
Eries Trisnadi <me@eries.id>
2017-11-05 21:46:10 +0700

    pdfmake typings.d.ts

---
+++ src/typings.d.ts
@@ -0,0 +1,4 @@
+// Typings reference file, you can add your own global typings here
+// https://www.typescriptlang.org/docs/handbook/writing-declaration-files.html
+declare module 'pdfmake/build/pdfmake.js';
+declare module 'pdfmake/build/vfs_fonts.js';
