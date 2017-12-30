421572cb8bacfc41ba952f369df9dbb8263fd3f1
Eries Trisnadi <me@eries.id>
2017-12-30 18:52:11 +0700

    update endpoint api

--- src/app/shared/classes/app.ts
+++ src/app/shared/classes/app.ts
@@ -1,6 +1,6 @@
 export class Config {
     name = 'Storaji';
     description = 'Manage your company stocks with us.';
-    version = 'v1.0.0-alpha';
-    api = 'https://storaji.herokuapp.com/api';
+    version = 'v1.0.0-beta.4';
+    api = 'http://180.250.111.28:7812/api';
 }
