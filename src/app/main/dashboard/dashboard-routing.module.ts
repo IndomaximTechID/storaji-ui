5c70fc7b35a4a544634cbdfafb5fd76bba1f5931
Eries Trisnadi <me@eries.id>
2018-02-11 23:59:09 +0700

    AoT fix for ChartModule

--- src/app/main/dashboard/dashboard-routing.module.ts
+++ src/app/main/dashboard/dashboard-routing.module.ts
@@ -1,27 +1,37 @@
 import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import * as Highcharts from 'highcharts';
+import { HighchartsStatic } from 'angular2-highcharts/dist/HighchartsService';
 import { ChartModule  } from 'angular2-highcharts';
 import { DashboardComponent } from './dashboard.component';
 import { SharedModule } from '../../shared/modules/shared.module';
 import { AuthGuard } from '../../core/services/auth.guard';
 import { OrdersRoutingModule } from '../orders/orders-routing.module';
 
-Highcharts.setOptions({
-  colors: ['#39f', '#ccc']
-});
+export function HighchartsFactory() {
+  Highcharts.setOptions({
+    colors: ['#39f', '#ccc']
+  });
+  return Highcharts;
+}
 
 @NgModule({
   imports: [
     SharedModule,
     OrdersRoutingModule,
-    ChartModule.forRoot(Highcharts),
+    ChartModule,
     RouterModule.forChild([
       { path: 'dashboard', component: DashboardComponent, canActivate: [AuthGuard] }
     ])
   ],
   declarations: [DashboardComponent],
-  providers: [AuthGuard],
+  providers: [
+    AuthGuard,
+    {
+      provide: HighchartsStatic,
+      useFactory: HighchartsFactory
+    }
+  ],
   exports: [
     RouterModule,
     DashboardComponent
